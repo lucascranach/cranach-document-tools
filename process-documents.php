a451d809a1a5b330c32fdad669d94e39b1c833f1
cnoss <christian.noss@th-koeln.de>
2022-05-12 14:10:15 +0200

    Documents will be extract now.

--- process-documents.php
+++ process-documents.php
@@ -8,17 +8,8 @@ in der documents.config angeben.
 
 ########################################################################  */
 
-$local_config = getConfig();
 $config = (object) [];
-
-// Bestehende JSONS überschreiben?
-setConfigValue('FORCE', true);
-setConfigValue('PERIOD', '-10000');
-setConfigValue('SOURCE', '/var/www/documents');
-setConfigValue('TARGET', '/var/www/documents');
-setConfigValue('BASEPATH', '/var/www/documents');
-setConfigValue('JSON_OUTPUT_FN', 'documentData-1.0.json');
-
+$config->LOCALCONFIG = getConfigFile();
 
 // Nach welchem Pattern soll gesucht werden?
 setConfigValue('PATTERN', '*.pdf');
@@ -43,12 +34,15 @@ $config->TYPES = $types;
 /* Functions
 ############################################################################ */
 
-function getConfig()
+function getConfigFile()
 {
-    $config_file = './documents.config';
+    $config_file = './document-tools-config.json';
     if (!file_exists($config_file)) {
-        return false;
+        print "--------------------\n";
+        print "Keine document-tools-config.json gefunden :(\n";
+        exit;
     }
+
     $config = file_get_contents($config_file);
     return json_decode(trim($config));
 }
@@ -86,14 +80,15 @@ class DocumentCollection
 
     public $documents = array();
 
-    public function __construct($config)
+    public function __construct($params)
     {
-        $this->config = $config;
+      global $config;
+        $this->params = $params;
 
-        $cmd = "find " . $this->config->SOURCE . " -maxdepth 4 -mtime ".$this->config->PERIOD ." -name '" . $this->config->PATTERN . "' ";
+        $cmd = "find " . $this->params["source"] . " -maxdepth 4 -mtime ". $this->params["period"] ." -name '" . $this->params["pattern"] . "' ";
         exec($cmd, $this->files);
-        
-        $pattern = "=" . $this->config->SOURCE . "=";
+
+        $pattern = "=" . $this->params["source"] . "=";
         $this->files = preg_replace($pattern, "", $this->files);
         
         $assets = array();
@@ -105,7 +100,7 @@ class DocumentCollection
             $res = [];
             $res["name"] = $assetBasePath;
             
-            foreach ($this->config->TYPES as $typeName => $typeData) {
+            foreach ($config->TYPES as $typeName => $typeData) {
                 $typePattern = getTypeSubfolderName($typeName);
                 $filenamePattern = getTypeFilenamePattern($typeName);
                 $searchPattern = (isset($filenamePattern)) ? $typePattern . "/" . $filenamePattern : $typePattern;
@@ -142,11 +137,12 @@ class DocumentBundle
 }
 
 
-function processDocuments($collection, $config)
+function processDocuments($collection, $params)
 {
-    $stackSize = $collection->getSize();
+  global $config;  
+  $stackSize = $collection->getSize();
     $count = 0;
-
+    
     foreach ($collection->documents as $document) {
 
         $documentName = $document["name"];
@@ -155,9 +151,9 @@ function processDocuments($collection, $config)
 
         print "\nAsset $count from $stackSize // $documentName:";
         $documentBundle = new DocumentBundle;
-        $jsonPath = $config->TARGET . "/$documentName/" . $config->JSON_OUTPUT_FN;
+        $jsonPath = $params["target"] . "/$documentName/" . $config->LOCALCONFIG->jsonOutputFilename;
 
-        if (file_exists($jsonPath) && !$config->FORCE) {
+        if (file_exists($jsonPath) && !$params["overwrite"]) {
             print "… already exists :)";
             continue;
         }
@@ -183,11 +179,204 @@ function processDocuments($collection, $config)
     }
 }
 
+function getConvertionParams($cliOptions, $params)
+{
+
+    $sourceBasePath = $params["sourceBasePath"];
+    $source = isset($cliOptions["dir"])
+    ? $sourceBasePath . '/' . $cliOptions["dir"]
+    : $sourceBasePath;
+
+    $targetBasePath = $params["targetBasePath"];
+    $target = isset($cliOptions["dir"])
+    ? $targetBasePath . '/' . $cliOptions["dir"]
+    : $targetBasePath;
+
+    $defaultPattern = gettype($params["pattern"]) === "array" ? implode("|", $params["pattern"]) : $params["pattern"];
+    $pattern = isset($cliOptions["pattern"])
+    ? $cliOptions["pattern"]
+    : $defaultPattern;
+
+    $period = isset($cliOptions["period"])
+    ? $cliOptions["period"]
+    : $params["defaultPeriod"];
+
+    $overwrite = isset($cliOptions["overwrite"])
+    ? $cliOptions["overwrite"]
+    : "nein";
+
+    $params = [
+        "sourceBasePath" => $sourceBasePath,
+        "source" => $source,
+        "targetBasePath" => $targetBasePath,
+        "target" => $target,
+        "pattern" => $pattern,
+        "period" => $period,
+        "overwrite" => $overwrite,
+    ];
+
+    return $params;
+}
+
+function getCliOptions()
+{
+    $ret = [];
+    $options = getopt("p:d:o:t:");
+    if (isset($options["p"])) {$ret["pattern"] = $options["p"];}
+    if (isset($options["d"])) {$ret["dir"] = $options["d"];}
+    if (isset($options["o"])) {$ret["overwrite"] = true;}
+    if (isset($options["t"])) {$ret["period"] = $options["t"];}
+
+    return $ret;
+}
+
+function confirmParams($params)
+{
+    print "----------\n";
+    print "Quellverzeichnis: " . $params["source"] . "\n";
+    print "Zielverzeichnis: " . $params["target"] . "\n";
+    print "Pattern: " . $params["pattern"] . "\n";
+    print "Zeitspanne: " . $params["period"] . "\n";
+    print "Overwrite: " . $params["overwrite"] . "\n";
+
+    print "\nAlle Angaben in Ordnung? [j,n] ";
+    $choice = rtrim(fgets(STDIN));
+
+    if ($choice !== 'j') {exitScript();}
+    return true;
+}
+
+function exitScript()
+{
+    print "\nfertig :)\n\n";
+    exit;
+}
+
+function extraxtDocuments($documentCollection, $params){
+
+  $target = $params["target"];
+  $source = $params["source"];
+
+  foreach($documentCollection->documents as $document){
+    $name = $document["name"];
+    $data = $document["data"];
+
+    foreach ($data as $key => $files) {
+      if(sizeof($files) === 0 ) continue;
+
+      foreach($files as $file){
+        $sourcePath = $source . "/" . $file;#
+        $targetPath = $target . "/" . $file;#
+        createRecursiveFolder($targetPath);
+
+        $cmd = "rsync $sourcePath $targetPath";
+        shell_exec($cmd);
+
+        print "$file\n";
+      }
+    }
+    
+    
+  }
+}
+
+function createRecursiveFolder($path)
+{
+    preg_match("=(.*)\/=", $path, $res);
+    $segments = explode("/", $res[1]);
+
+    $growingPath = [];
+    foreach ($segments as $segment) {
+        array_push($growingPath, $segment);
+        $newPath = implode("/", $growingPath);
+        if (preg_match("=[a-zA-Z]=", $newPath) && !file_exists($newPath)) {mkdir($newPath, 0775);}
+    }
+    return;
+}
+
+function showMainMenu($config)
+{
+
+    $actions = [
+        "extraxt-and-move-pdfs" => "PDFs aus Verzeichnisstruktur extrahieren und in «documents» Folder packen.",
+        "generate-json" => "JSON Dateien erzeugen",
+        "exit" => "Skript beenden",
+    ];
+
+    $params = [
+        "-p" => "Übergibt ein File-Pattern, welches das Pattern der Config überschreibt, z.B. -p \"G_*\"",
+        "-t" => "Übergibt ein Periode, z.B. -2 findet nur Dateien, die in den letzten 2 Tagen geändert wurden."
+    ];
+
+    print "\n#############################################################################\n";
+    print "Cranach Document Tools\n\n";
+    print "Das Skript kann mit folgenden Parametern aufgerufen werden:\n";
+    foreach ($params as $key => $value) {
+        print "$key\t$value\n";
+    }
+    print "\nFolgende Aktionen sind verfügbar:\n";
+
+    $count = 0;
+    $options = [];
+
+    foreach ($actions as $key => $value) {
+        $count++;
+        print "[$count] $value\n";
+        $options[$count] = $key;
+    }
+
+    print "\nWas soll gemacht werden? ";
+    $choice = intval(rtrim(fgets(STDIN)));
+    if (!array_key_exists($choice, $options)) {print "\nDiese Aktion ist nicht verfügbar.\n\n";exit;}
+
+    switch ($options[$choice]) {
+
+      case "extraxt-and-move-pdfs":
+        print "\nPDFs aus Verzeichnisstruktur extrahieren und in «documents» Folder packen …\n";
+
+        $cliOptions = getCliOptions($config);
+        $params = getConvertionParams($cliOptions, [
+            "sourceBasePath" => $config->LOCALCONFIG->source,
+            "targetBasePath" => $config->LOCALCONFIG->target,
+            "pattern" => ["*.pdf"],
+            "defaultPeriod" => $config->LOCALCONFIG->defaultPeriod,
+        ]);
+
+        confirmParams($params);
+        $documentCollection = new DocumentCollection($params);
+        extraxtDocuments($documentCollection, $params);
+        exitScript();
+        break;
+
+        case "generate-json":
+          print "\nJSON Dateien erzeugen …\n";
+
+          $cliOptions = getCliOptions($config);
+          $params = getConvertionParams($cliOptions, [
+              "sourceBasePath" => $config->LOCALCONFIG->target,
+              "targetBasePath" => $config->LOCALCONFIG->target,
+              "pattern" => ["*.pdf"],
+              "defaultPeriod" => $config->LOCALCONFIG->defaultPeriod,
+          ]);
+
+          confirmParams($params);
+          $documentCollection = new DocumentCollection($params);
+          processDocuments($documentCollection, $params);
+          exitScript();
+          break;
+
+        case "exit":
+            exitScript();
+            break;
+
+        default:
+            print "Na gut, dann eben nix.\n";
+            exit;
+    }
+
+}
+
 /* Main
 ############################################################################ */
 
-$newSession = "\n#######################################################\n" . date("d.m.Y, H:i:s", time());
-addLogEntry($newSession);
-
-$documentCollection = new DocumentCollection($config);
-processDocuments($documentCollection, $config);
+showMainMenu($config);
